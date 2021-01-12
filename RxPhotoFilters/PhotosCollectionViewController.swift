dd56b600cbbd70666700f6914410a064d6c2af67
Alexander Milgunov <nagvalbox@bk.ru>
2021-01-12 23:37:36 +0500

    adding pods

--- RxPhotoFilters/PhotosCollectionViewController.swift
+++ RxPhotoFilters/PhotosCollectionViewController.swift
@@ -7,7 +7,23 @@
 
 import Foundation
 import UIKit
+import Photos
 
 class PhotosCollectionViewController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        populatePhotos()
+    }
+    
+    private func populatePhotos() {
+        
+        PHPhotoLibrary.requestAuthorization { status in
+            if status == .authorized {
+                
+            }
+        }
+        
+    }
     
 }
